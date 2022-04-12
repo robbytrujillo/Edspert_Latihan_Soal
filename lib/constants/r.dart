eb549ba250caa2d13591bfffba66c91a24d5acc0
RUANGIT <robbytrujillo>
2022-04-12 10:19:45 +0700

    Login Page & Splash Screen

---
+++ lib/constants/r.dart
@@ -0,0 +1,9 @@
+import 'package:git_intro/constants/R/app_colors.dart';
+import 'package:git_intro/constants/R/assets.dart';
+import 'package:git_intro/constants/R/strings.dart';
+
+class R {
+  static AppColor colors = AppColor();
+  static Assets assets = Assets();
+  static Strings strings = Strings();
+}
--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -1,11 +1,20 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:git_intro/constants/r.dart';
 import 'package:git_intro/view/login_page.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
+  static String route = "splash_screen";
 
   @override
   Widget build(BuildContext context) {
+    Timer(const Duration(seconds: 5), () {
+      //Navigator.of(context)
+      //    .push(MaterialPageRoute(builder: (context) => LoginPage()));
+      Navigator.of(context).pushNamed(LoginPage.route);
+    });
     return Scaffold(
       //  appBar: AppBar(),//parameter appBar menerima sebuah widget appBar juga
       body: GestureDetector(
@@ -16,12 +25,12 @@ class SplashScreen extends StatelessWidget {
         },
         child: Center(
           child: Image.asset(
-            "assets/auth/ic_splash.png",
+            R.assets.icSplash,
             width: MediaQuery.of(context).size.width * 0.5,
           ),
         ),
       ), //membuat sebuah image menggunakan body
-      backgroundColor: Color(0xff3A7FD5),
+      backgroundColor: R.colors.primary,
     );
   }
 }
