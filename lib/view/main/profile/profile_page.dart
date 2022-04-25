b72f4f814d8c4ed677b30bdf0fc4c613fb49721a
RUANGIT <robbytrujillo>
2022-04-25 10:14:40 +0700

    Halaman Profile

--- lib/view/main/profile/profile_page.dart
+++ lib/view/main/profile/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:git_intro/constants/r.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -11,9 +12,68 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("Akun Saya"),
+        centerTitle: true,
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              "Edit",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+      ),
       body: Column(
-        children: [Text("PROFILE")],
+        children: [
+          Container(
+            padding: EdgeInsets.only(
+              top: 28,
+              bottom: 60,
+              right: 15,
+              left: 15,
+            ),
+            decoration: BoxDecoration(
+                color: R.colors.primary,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(9),
+                  bottomRight: Radius.circular(9),
+                )),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Nama User",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      Text(
+                        "Nama Sekolah User",
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Image.asset(
+                  R.assets.imgUser,
+                  width: 50,
+                  height: 50,
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
